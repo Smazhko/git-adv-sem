01db15c2f8d791b63948b044d9d36cc270bc3973
Peter Smazhko <smazhko@gmail.com>
2023-10-23 12:44:33 +0300

    main.js added

---
+++ main.js
@@ -0,0 +1,34 @@
+(function(){
+    let countSpaces = function(text) {
+        let matches = text.matchAll(/\s/g);
+        let count = 0;
+        for(let match of matches) {
+            count++;
+        }
+        return count;
+    }
+
+    document.addEventListener('DOMContentLoaded', function(){
+        let textArea = document.querySelector('#text');
+        let result = document.querySelector('#result');
+        let countButton = document.querySelector('#count-button');
+        let resetButton = document.querySelector('#reset-button');
+
+        countButton.addEventListener('click', function(){
+            let text = textArea.value;
+            let withoutSpaces = text.length - countSpaces(text);
+            result.innerHTML = '<b>Длина текста:</b> ' +
+                text.length + ' символов<br>' +
+                '<b>Без пробелов:</b> ' + withoutSpaces + '<br>';
+        });
+
+        textArea.addEventListener('keyup', function(){
+            changeButtonsStatus(textArea.value.length > 0);
+        });
+
+        resetButton.addEventListener('click', function(){
+            textArea.value = '';
+            changeButtonsStatus(false);
+        });
+    });
+}());
